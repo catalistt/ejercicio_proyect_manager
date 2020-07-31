b33b0d18a8d03175274f7c97ed6fbc07a74d956f
Catalina <caliste@fen.uchile.cl>
2020-07-30 22:06:23 -0400

    second commit

---
+++ app/controllers/proyects_controller.rb
@@ -0,0 +1,30 @@
+class ProyectsController < ApplicationController
+
+    http_basic_authenticate_with name: "dhh", password: "secret", except: :index
+
+  def new
+    @proyect = Proyect.new
+  end
+
+  def index
+    if params[:search]
+      @proyects = Proyect.where(status: params[:search])
+
+    else 
+      @proyects = Proyect.all
+    end
+  end
+
+  def create
+    puts params
+     @proyect = Proyect.new(name: params[:proyect][:name],description: params[:proyect][:description], start_date: params[:proyect][:start_date], end_date: params[:proyect][:end_date], status: params[:proyect][:status])
+
+    if @proyect.save
+      redirect_to proyects_new_path
+    else
+      flash[:error] = "Ocurrió un error guardando el proyecto"
+      redirect_to proyects_new_path
+    end 
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'proyects/new'
+  post 'proyects/create'
+  root 'proyects#new'
+  get 'proyects/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
