97e203ce610a64a721c19c0ac86f523642e2b4b1
Catalina <caliste@fen.uchile.cl>
2020-08-02 19:17:31 -0400

    final 1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,10 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.assets.digest = true
+  config.serve_static_assets = true
   # Code is not reloaded between requests.
   config.cache_classes = true
+  
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
